31ab79262e40e1221513acc82d9489ab9e0dc885
Kobrius11 <kobrius11@gmail.com>
2024-01-28 13:30:36 +0200

    asd

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    public float num;
+    public float cooldown = 8;
+    public bool isCooldown = false;
+    public GameObject charecter;
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        num += 1 / cooldown * Time.deltaTime;
+        if (num >= 60)
+        {
+            num = 0;
+            isCooldown = false;
+        }
     }
 }
